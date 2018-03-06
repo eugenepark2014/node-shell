61219ef759f0aa84e910625b698e4672a2021a7f
eugenepark2014 <eugenepark2014@gmail.com>
2018-03-06 12:23:51 -0500

    switch1

---
+++ bash.js
@@ -0,0 +1,13 @@
+const pwd = require("./pwd")
+
+process.stdout.write('prompt > ');
+process.stdin.on('data', (data) =>{
+  const cmd = data.toString().trim();
+  if(cmd  === 'pwd'){
+    pwd();
+  }
+  else{
+  process.stdout.write('You typed: ' + cmd + '\n');
+  }
+  process.stdout.write('prompt > ');
+});
---
+++ pwd.js
@@ -0,0 +1,1 @@
+module.exports = function(){console.log(`current directory: ${process.cwd()}`);}
